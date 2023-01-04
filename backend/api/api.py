e0fb3bdf613c5cfb861b4b29142ccc3a07634714
cstainsby <cstainsby@zagmail.gonzaga.edu>
2023-01-03 23:09:52 -0800

    backend basic setup complete

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,5 +1,26 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
-from .database import PenteDatabase
+from .database.database import PenteDatabase
 from . import models
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def test():
+  return "hi"
