f1c0dad6623cf9b1243073abf58fae5fa87bec61
cstainsby <cstainsby@zagmail.gonzaga.edu>
2023-01-11 16:18:40 -0800

    building in game board for frontend

--- frontend/src/pages/GamePage.js
+++ frontend/src/pages/GamePage.js
@@ -6,8 +6,9 @@ const GamePage = (props) => {
 
   return (
     <div className="GamePage">
-      In Game
+      <GameSidebar/>
       <Outlet/>
+      <GameLog/>
     </div>
   );
 }
@@ -16,7 +17,33 @@ const GameSidebar = (props) => {
 
   return (
     <div className="GameSidebar"> 
+      <PlayerTag playerName="AI"/>
+    </div>
+  )
+}
 
+const PlayerTag = (props) => {
+  // props 
+  //  player name 
+  //  number of captures
+
+  // const tagElement = document.getElementById("PlayerTag");
+  // tagElement.style.backgroundColor = "#00FF00"
+  return (
+    <div id="PlayerTag">
+      <h3>{ props.playerName }</h3>
+    </div>
+  )
+}
+
+const GameLog = (props) => {
+
+  return (
+    <div id="GameLog">
+      <h3>Game Log</h3>
+      <div id="GameLogContents">
+
+      </div>
     </div>
   )
 }
@@ -30,13 +57,43 @@ const GameDisplay = (props) => {
   console.log(gameState)
   console.log(gameState.length)
 
+  const boardMargin = 20; // how much margin is around the board 
+  const offsetFromOrigin = boardMargin / 2; // how much each item within the board needs to be offset
+
+  const gameDisplayLength = window.screen.height / 1.5;
+  const boardDisplayLength = gameDisplayLength - boardMargin;
+  // line X and Ys will hold all info to 
+  const lineIntervalSpacing = boardDisplayLength / gridLength;
+  let helperCount = Array.from({ length: gridLength + 1 }, (_, i) => i);
+  const lineXs = helperCount.map(x => x * lineIntervalSpacing);
+  const lineYs = helperCount.map(y => y * lineIntervalSpacing);
+
+  // parallel to the game state, holds all x and y intersections relative to the grid
+  const placeableIntersections = []
+
+
+
+
   return (
     <div className="GameDisplay">
-      <svg width="400" height="100">
-        <rect width="400" height="100" />
+      <svg width={ gameDisplayLength } height={ gameDisplayLength }>
+        <rect width={ gameDisplayLength } height={ gameDisplayLength } fill="green" />
+
+        <rect x={ offsetFromOrigin } y={ offsetFromOrigin } width={ boardDisplayLength } height={ boardDisplayLength } fill="white" />
+        {/* place lines */}
+        { lineXs.map(x =>
+          <line x1={x + offsetFromOrigin} x2={x + offsetFromOrigin} y1={offsetFromOrigin} y2={ boardDisplayLength + offsetFromOrigin } stroke="black" strokeWidth={2}/>
+        )}
+        { lineYs.map(y =>
+          <line x1={offsetFromOrigin} x2={ boardDisplayLength + offsetFromOrigin } y1={y + offsetFromOrigin} y2={y + offsetFromOrigin} stroke="black" strokeWidth={2}/>
+        )}
       </svg>
     </div>
   )
+}
+
+const GameIntersection = (props) => {
+
 }
 
 const OnlineGameDisplay = (props) => {
@@ -46,8 +103,7 @@ const OnlineGameDisplay = (props) => {
 const AiGameDisplay = (props) => {
   console.log("Ai game loaded")
   return (
-    <div>
-      AI Game
+    <div id="AiGameDisplay">
       <GameDisplay/>
     </div>
   )
